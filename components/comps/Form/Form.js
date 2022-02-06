bb8bdeeed6e04062d177e8e910e4497380d9eea2
Detola Oyawoye <oyawoye.ade@gmail.com>
2022-02-06 23:46:38 +0000

    [wip] - fix form composition

--- components/comps/Form/Form.js
+++ components/comps/Form/Form.js
@@ -11,9 +11,9 @@ export function Form({ className = "", inputs }) {
     console.log("INput: ", inputs)
     return (
         <form className={`` + className}>
-            {inputs.map(item => {
+            {inputs.map(item => 
                 <Input key={item._id} label={item.name} {...item} />
-            })}
+            )}
             <Input label="first name" />
             <Button type="submit">Subscribe</Button>
         </form>
